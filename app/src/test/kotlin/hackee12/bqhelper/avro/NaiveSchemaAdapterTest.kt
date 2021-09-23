9fb16845e55d43a89ef88ec39867fca92498bd9d
hackee12 <hackee.owl@gmail.com>
2021-09-23 12:09:54 -0400

    Add NaiveSchemaAdapterTest

---
+++ app/src/test/kotlin/hackee12/bqhelper/avro/NaiveSchemaAdapterTest.kt
@@ -0,0 +1,22 @@
+package hackee12.bqhelper.avro
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.api.services.bigquery.model.TableFieldSchema
+import org.apache.avro.Schema
+import java.io.File
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class NaiveSchemaAdapterTest {
+
+    @Test
+    fun test() {
+        val avroSchema = Schema.Parser().parse(File("src/test/resources/user.avsc"))
+        val expected: MutableList<TableFieldSchema> = ObjectMapper().readValue(
+            File("src/test/resources/user.table.json").readText(),
+            mutableListOf<TableFieldSchema>().javaClass
+        )
+        val actual: List<TableFieldSchema> = NaiveSchemaAdapter().asTableFields("_ignore_", avroSchema)[0].fields
+        assertEquals(expected, actual)
+    }
+}
